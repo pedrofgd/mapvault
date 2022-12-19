9dc9e0ccad404e1dc3003fe05331b3d4fe1b792b
Pedro Dias <mnopedrodias@gmail.com>
2022-12-19 17:58:37 -0300

    web: impl editar nota e botao para deletar

--- web/pages/note/[id].tsx
+++ web/pages/note/[id].tsx
@@ -2,21 +2,65 @@ import { useRouter } from "next/router"
 import { Note } from "../../interfaces"
 import useSwr from 'swr'
 import styles from '../../styles/Home.module.css'
+import Editor from "../../components/editor"
 import Navbar from "../../components/navbar"
 import Footer from "../../components/footer"
+import { useState } from "react"
+import { useEditor } from "../../contexts/editor"
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function NotePage() {
    const router = useRouter()
 
+   const { content, setContent } = useEditor()
+   const [editToogle, setEditToogle] = useState(false)
+
+   function handleEditToogle() {
+      setEditToogle(!editToogle)
+   }
+
+   function handleEditNote() {
+      console.log("ok")
+      console.log(content)
+   }
+
    const { data, error } = useSwr<Note>(`/api/notes/${router.query.id}`, fetcher)
 
    if (error) return <div>Failed to load users</div>
    if (!data) return <div>Loading...</div>
 
-   var parse = require('html-react-parser')
-   var content = parse (data.content)
+   setContent(data.content)
+
+   function ViewNoteBlock(content: string) {
+      var parse = require('html-react-parser')
+   
+      return (
+         <div className="card-body py-0">
+            <div className="quill my-2">
+               <div className="ql-snow">
+                  <div className="ql-editor">
+                     {parse(content)}
+                  </div>
+               </div>
+            </div>
+         </div>
+      )
+   }
+
+   function EditorBlock(content: string) {
+      return (
+         <div className="p-3">
+            <Editor defaultContent={content} />
+            <div className="d-flex justify-content-end mt-2">
+               <button type="button" className="me-2 btn btn-outline-secondary btn-sm"
+                  onClick={handleEditToogle}>Cancel</button>
+               <button type="button" className="btn btn-success btn-sm"
+                  onClick={handleEditNote}>Success</button>
+            </div>
+         </div>
+      )
+   }
 
    return (
       <div className={styles.container}>
@@ -26,7 +70,7 @@ export default function NotePage() {
             <div className="row">
                <div className="col-8">
                   <h1 style={{width: "100%", overflowWrap: "break-word"}}>{data.title}</h1>
-                  <h4>{data.exceptionMessage}</h4>
+                  <h4 className="fw-light">{data.exceptionMessage}</h4>
                   {data.categories?.map((category) => {
                      return (
                         <span key={category} className="badge text-bg-dark p-2 m-1">
@@ -36,33 +80,31 @@ export default function NotePage() {
                   })}
 
                   <div className="card mt-4">
-                     <div className="card-header">
+                     <div className="card-header py-1">
                         <div className="d-flex justify-content-between align-items-center">
-                           <div className="fw-light">
+                           <div className="fw-light" style={{fontSize: "14px"}}>
                               <span className="text-muted">última modificação: {` `}</span>
                               <span>19 dezembro 2022</span>
 
                            </div>
                            <div className="dropdown">
-                           <button className="btn btn-sm btn-outline-dark" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                           <button className="btn btn-sm" type="button" 
+                              data-bs-toggle="dropdown" aria-expanded="false">
                               ...
                            </button>
                            <ul className="dropdown-menu">
-                              <li><a className="dropdown-item" href="#">Editar</a></li>
+                              <li><button className="dropdown-item" onClick={handleEditToogle}>Editar</button></li>
+                              <li><hr className="dropdown-divider"/></li>
+                              <li><h6 className="dropdown-header">Ações na nota</h6></li>
+                              <li><a className="dropdown-item text-danger" href="#">Deletar tudo</a></li>
                            </ul>
                            </div>
                         </div>
                      </div>
 
-                     <div className="card-body py-0">
-                        <div className="quill my-4">
-                           <div className="ql-snow">
-                              <div className="ql-editor">
-                                 {content}
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+                     {editToogle == true 
+                        ? EditorBlock(data.content)
+                        : ViewNoteBlock(data.content) }
                   </div>
                </div>
 
