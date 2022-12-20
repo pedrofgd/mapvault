22754932d67bb653c14ffe7c3d08df65cfd8b18d
Pedro Dias <mnopedrodias@gmail.com>
2022-12-19 21:20:57 -0300

    web: integrar API DELETE /api/v1/notes/{id}

---
+++ web/pages/api/notes/[id].ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function noteHandler(req: NextApiRequest, res: NextApiResponse) {
+   const { 
+      query: {id},
+      method
+   } = req
+
+   switch (method) {
+      case 'GET':
+         const notes = await axios.get(`http://localhost:5149/api/v1/notes/${id}`);
+         res.json(notes.data)
+         break
+      case 'DELETE':
+         axios.delete(`http://localhost:5149/api/v1/notes/${id}`)
+         .then(response => {
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json');
+            res.setHeader('Cache-Control', 'max-age=180000');
+            res.end(JSON.stringify(response))
+          })
+          .catch(error => {
+            res.json(error);
+            res.status(405).end();
+          });
+          break
+          default:
+      res.setHeader('Allow', ['GET', 'DELETE'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+   }
+
+}
