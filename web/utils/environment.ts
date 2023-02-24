3b898c387f0053d1da81cec2d25e923a5b1331b4
Pedro Dias <mnopedrodias@gmail.com>
2023-02-24 17:10:52 -0300

    web: nome do ambiente na navbar

--- web/utils/environment.ts
+++ web/utils/environment.ts
@@ -2,3 +2,17 @@ export function IsDevelopment() {
    var environment = process.env.NODE_ENV?.toString()!
    return new RegExp('development', 'gi').test(environment)
 }
+
+export function GetShortEnvironmentName() {
+   var environment = process.env.NODE_ENV
+   switch (environment) {
+      case 'development':
+         return 'dev'
+   
+      case 'production':
+         return 'prod'
+
+      default:
+         return environment
+   }
+}
