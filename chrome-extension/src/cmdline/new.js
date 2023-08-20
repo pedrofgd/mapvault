cf28dabe3c59205c470a93f60d7fffb40333d9e4
Pedro Dias <mnopedrodias@gmail.com>
2023-08-20 20:33:32 -0300

    chrome: make alias arg optional for command new

--- chrome-extension/src/cmdline/new.js
+++ chrome-extension/src/cmdline/new.js
@@ -4,14 +4,26 @@ import { storeNoteId } from "../storage.js";
 
 export async function newCommand(arg) {
     // TODO: parse arguments and flags
-    const args = arg.split("--");
+    const args = arg.split("-");
     const nameArgument = args[0];
-    const aliasArgument = args[1];
-    const alias = aliasArgument.split(" ")[1];
-    console.log(alias);
+
+    const parsedArguments = parseArguments(args.slice(1));
+
+    const alias = parsedArguments["a"] ?? null;
 
     const note = await createNote(nameArgument, alias, location);
     storeNoteId(note.id, host, pathname);
 
     console.log(note);
+}
+
+function parseArguments(args) {
+    let parsedArgs = {};
+    for (let i = 0; i < args.length; i++) {
+        const segments = args[i].split(/\s(.*)/s);
+        const argName = segments[0].trim();
+        parsedArgs[argName] = segments[1];
+    }
+
+    return parsedArgs;
 }
